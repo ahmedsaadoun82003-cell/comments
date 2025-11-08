3d0567d78c5df82ebc4af798ca8c0a5c9a01f77a
ahmedsaadoun82003-cell <ahmedsaadoun82003@gmail.com>
2025-11-08 21:39:52 +0200

    Update and rename comments.txt to comments.py

---
+++ comments.py
@@ -0,0 +1,8 @@
+{
+    "api_url": "https://smmegyfans.com/adminapi/v2",
+    "api_key": "y0eikgcco647g5nzohpkh56fr1ijj5lsfuj9poh2zye1fwksungrsc4ajrgybrq4",
+    "COMMENT_THREADS": 1,
+    "CHECK_INTERVAL": 30,
+    "LANGUAGE": "ar"
+}
+
