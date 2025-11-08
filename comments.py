445a6e4bea7445b822f9c1fa4bc9f8620db9742c
ahmedsaadoun82003-cell <ahmedsaadoun82003@gmail.com>
2025-11-08 21:40:24 +0200

    Update comments.py

--- comments.py
+++ comments.py
@@ -1,8 +1,6 @@
 {
-    "api_url": "https://smmegyfans.com/adminapi/v2",
-    "api_key": "y0eikgcco647g5nzohpkh56fr1ijj5lsfuj9poh2zye1fwksungrsc4ajrgybrq4",
-    "COMMENT_THREADS": 1,
-    "CHECK_INTERVAL": 30,
-    "LANGUAGE": "ar"
+    "activation_key": "KKLIM-NQWSG-FANKP-NRHBT",
+    "version": "1.1.0",
+    "language": "en",
+    "check_updates_on_start": "false"
 }
-
