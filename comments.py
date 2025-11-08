0e5800c836e5761a22a0ada929bbd3aebf0d7083
ahmedsaadoun82003-cell <ahmedsaadoun82003@gmail.com>
2025-11-08 21:42:39 +0200

    Update comments.py

--- comments.py
+++ comments.py
@@ -1,6 +1,6 @@
 {
-    "activation_key": "KKLIM-NQWSG-FANKP-NRHBT",
-    "version": "1.1.0",
-    "language": "en",
-    "check_updates_on_start": "false"
+    
+    "product_id": 31485,
+    "RSAPubKey": "<RSAKeyValue><Modulus>sCodhAfhHwa8hSl7tmF6C2sovIwF8V+rBosa1xoYm/nZDtPlbI9U+gjxKKRG/tzf8mB1kEnO5vAC5bsXfGvYL5Cit3VRa1+ZxOSVUY+gyp2HFVtsT+xKMaLUvn4kYUWowaNoXO2x/VRKR0Fdz6hw6Oarx30cPAxjELowHrKy227XYBTj4VqgPqn+GtOz8L+7/8KFSxeg0HIqp3fe9I5Xzus/pKWpLkTeDC71HW8M2ZX6J5r4bVYUX60GihV7eN30dIIl0Wdtenx3CbtHFhzflvEWnVMqgZNzYf1Cou6Z3Xpjef5354l4uEHHxQRfMGpwtjG0ecfkhjuRX8MjOgtQ4Q==</Modulus><Exponent>AQAB</Exponent></RSAKeyValue>",
+    "auth": "WyIxMTQzOTE1MzUiLCJQSGRoT0tMdFNCWWRmb3R5YVBsS1lXcS9FVzIrUkhXL1gyMTlKZXhrIl0="
 }
